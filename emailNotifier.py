7d915c996c6e912a98dcc158ea5fde728c768fcb
Alexander Shen <70786647+shenalexw@users.noreply.github.com>
2022-03-07 21:30:00 -0800

    First Version

--- emailNotifier.py
+++ emailNotifier.py
@@ -1,8 +1,12 @@
 import os
+import sys
 import smtplib
 import ssl
 from dotenv import load_dotenv
 
+"""
+Open a terminal and use python -m smtpd -c DebuggingServer -n localhost:1025 if working on local
+"""
 # Decision Booleans
 onlineBOOL = True
 
@@ -14,6 +18,7 @@ PASSWORD = os.getenv('PASSWORD')
 # Load the email
 sender_email = os.getenv('USERNAME')
 receiver_email = os.getenv('SEND')
+url = sys.argv[1]
 message = """\
 Subject: Your Product is In-Stock!
 
@@ -22,7 +27,8 @@ Hello!
 The product you have been looking for is now in stock.
 
 Follow the link below to go buy it now!
-"""
+
+""" + url
 
 
 def main():
@@ -38,7 +44,7 @@ def main():
         with smtplib.SMTP_SSL(server, port, context=context) as server:
             server.login(USERNAME, PASSWORD)
             server.sendmail(sender_email, receiver_email, message)
-            print("E-mail has been sent")
+            print("E-mail has been sent online")
             server.close()
     else:
         server = "localhost"
@@ -47,8 +53,12 @@ def main():
         # login and send the email
         with smtplib.SMTP(server, port) as server:
             server.sendmail(sender_email, receiver_email, message)
-            print("E-mail has been sent")
+            print("E-mail has been sent offline")
             server.close()
+
+
+def changeUrl(newUrl):
+    url = newUrl
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,8 +1,97 @@
 import os
+import time
+import requests
+import validators
+from bs4 import BeautifulSoup
+
+"""
+Currently made to search for items in stock at Zara
+
+Example Link: https://www.zara.com/us/en/plaid-blazer-p02010745.html
+"""
 
 
 def main():
-    os.system('python3 emailNotifier.py')
+    headers = {
+        'user-agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
+    }
+    print("Email Stock Notifier for Zara")
+    url = input("Input the url for the item: ")
+    validateUrl(url)
+    size = input("Input the size of the item (xs/s/m/l/xl): ")
+    validateSize(size)
+    while True:
+        response = requestURL(
+            headers, url)
+        soup = getSoup(response)
+        entry = findElement(
+            soup, "span", "product-detail-size-info__main-label")
+        if determineStock(entry):
+            print("The Product is in stock")
+            os.system('python3 emailNotifier.py ' + url)
+            break
+        else:
+            print("The product is out of stock")
+        time.sleep(60)
+
+
+def validateUrl(url):
+    if not validators.url(url):
+        print("Not a valid URL")
+        exit()
+    if not url.find("zara"):
+        print("Not a zara link")
+        exit()
+
+
+def validateSize(size):
+    valid = False
+    if 0 < len(size) < 4:
+        valid = True
+
+    if size.lower() == "xs" or size.lower() == "s" or size.lower() == "m" or size.lower() == "l" or size.lower() == "xl" or size.lower() == "xl":
+        valid = True
+    else:
+        valid = False
+
+    if valid == False:
+        print("Not a valid size")
+        exit()
+
+
+def requestURL(headers, url):
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response
+    else:
+        print("URL was not able to be requested")
+        exit()
+
+
+def getSoup(response):
+    soup = BeautifulSoup(response.content, 'html.parser')
+    return soup
+
+
+def findElement(soup, htmlElement, classEntry):
+    divs = soup.find_all(
+        htmlElement, {"class": classEntry})
+    for entries in divs:
+        if entries.getText() == "M":
+            entry = entries
+            return entry
+    print("The Element was not found")
+    exit()
+
+
+def determineStock(entry):
+    parent = entry.parent.parent.parent
+    stringParent = str(parent)
+    if (stringParent.find('out-of-stock') != -1):
+        return False
+    else:
+        return True
 
 
 if __name__ == "__main__":
